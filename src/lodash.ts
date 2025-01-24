ebbe44b47393412dce4fba1b2f83392d532780ce
刘 相卿 <lxqmoma@outlook.com>
2025-01-24 13:44:13 +0800

    feat: 引入lodash.get lodash.has

--- src/lodash.ts
+++ src/lodash.ts
@@ -2,5 +2,7 @@ import cloneDeep from 'lodash.clonedeep'
 import debounce from 'lodash.debounce'
 import merge from 'lodash.merge'
 import clone from 'lodash.clone'
+import get from 'lodash.get'
+import has from 'lodash.has'
 
-export { cloneDeep, merge, debounce, clone }
+export { cloneDeep, merge, debounce, clone, get, has }
