b71c2fb721e088aa5563b7b4473039f7be311e88
刘 相卿 <lxqmoma@outlook.com>
2025-02-03 17:39:28 +0800

    docs: import`lodash.set`

--- src/lodash.ts
+++ src/lodash.ts
@@ -3,6 +3,7 @@ import debounce from 'lodash.debounce'
 import merge from 'lodash.merge'
 import clone from 'lodash.clone'
 import get from 'lodash.get'
+import set from 'lodash.set'
 import has from 'lodash.has'
 
-export { cloneDeep, merge, debounce, clone, get, has }
+export { cloneDeep, merge, debounce, clone, get, has, set }
