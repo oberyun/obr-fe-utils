0788be1d8e2b1e3cd479e54978af2388bf27910a
刘 相卿 <lxqmoma@outlook.com>
2026-01-07 14:10:38 +0800

    refactor: lodash.xxx -> es-toolkit

--- src/lodash.ts
+++ src/lodash.ts
@@ -1,11 +1,3 @@
-import cloneDeep from 'lodash.clonedeep'
-import debounce from 'lodash.debounce'
-import merge from 'lodash.merge'
-import clone from 'lodash.clone'
-import get from 'lodash.get'
-import set from 'lodash.set'
-import has from 'lodash.has'
-import kebabcase from 'lodash.kebabcase'
-import camelcase from 'lodash.camelcase'
+import { camelCase, clone, cloneDeep, debounce, get, has, kebabCase, merge, set } from 'es-toolkit/compat'
 
-export { cloneDeep, merge, debounce, clone, get, has, set, kebabcase, camelcase }
+export { cloneDeep, merge, debounce, clone, get, has, set, kebabCase, camelCase }
